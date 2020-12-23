44cb106cec096536519bf188df2ef1c2e9c599a4
grace179 <72989367+grace179@users.noreply.github.com>
2020-12-23 18:31:59 +0900

    Instagram에 Tag모델 추가, admin에 등록

--- instagram/admin.py
+++ instagram/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.utils.safestring import mark_safe
 
 # Register your models here.
-from .models import Post, Comment
+from .models import Post, Comment, Tag
 
 # admin.site.register(Post)
 @admin.register(Post)
@@ -22,4 +22,8 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+  pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
   pass
