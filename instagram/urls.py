d7bf2d605d3c34198d961487e2ce71a23f8ccef3
grace179 <72989367+grace179@users.noreply.github.com>
2020-12-24 18:04:40 +0900

    Instagram에 post_detail페이지 생성

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,6 +1,22 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+
+class YearConverter:
+  regex = "20\d{2}"
+
+  def to_python(self, value):
+    return int(value)
+
+  def to_url(self, value):
+    return str(value)
+
+register_converter(YearConverter, 'year')
+
+app_name = 'instagram' # URL Reverse에서 namespace역할을 하게 됨
 
 urlpatterns = [
   path('', views.post_list),
+  path('<int:pk>/', views.post_detail),
+  # path('archives/<int:year>/', views.archives_year),
+  path('archives/<year:year>/', views.archives_year),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.http import HttpResponse, HttpRequest, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
+# post_list = ListView.as_view(model = Post)
 def post_list(request):
   qs = Post.objects.all()
   q = request.GET.get('q', '')
@@ -12,3 +15,16 @@ def post_list(request):
     'post_list' : qs,
     'q':q,
   })
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+  post = get_object_or_404(Post, pk=pk)
+  # try:
+  #   post = Post.objects.get(pk=pk)
+  # except Post.DoesNotExist:
+  #   raise Http404
+  return render(request, 'instagram/post_detail.html',{
+    'post' : post,
+  })
+
+def archives_year(request, year):
+  return HttpResponse(f"{year}년 archives")
